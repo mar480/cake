d6acd15eb1b343854d694e50142817e191aec9b0
robertmarks <rmarks@companieshouse.gov.uk>
2026-04-07 11:50:03 +0100

    update .gitignore to remove previous locked version of app

--- app/frontend/src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-import XBRLTaxonomyExplorerContainer from '@/components/taxonomy/explorer/XBRLTaxonomyExplorerContainer';
-
-const Index = () => {
-  return <XBRLTaxonomyExplorerContainer />;
-};
-
-export default Index;
